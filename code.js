8e228b476aa2bd5f8dc7827f3511b7432e776ea4
Hamza <hamzawritescode@gmail.com>
2025-04-15 16:16:41 +0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,27 @@
+// Microsoft Kills People At Gaza
+
+
+let target = true;
+
+function kill() {
+    console.log("Target neutralized.");
+}
+
+function fire() {
+    if (target === true) {
+        kill();
+    } else {
+        console.log("No target acquired.");
+    }
+}
+
+fire();
+
+// This isn't just code.
+// It's a weapon!!
+
+
+// When tech chooses sides,
+// it becomes part of the war.
+
+// Microsoft MUST stop cooperarting with Isreal right now!!
